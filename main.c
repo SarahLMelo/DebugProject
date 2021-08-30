f09fab9273afbd9ecb1e86d7c7d1451774288e14
Junior Marcondes <jrmarcondesb@gmail.com>
2021-08-30 01:13:34 -0300

    buy fix

--- main.c
+++ main.c
@@ -25,8 +25,8 @@ double oldangulo;
 int main()
 {
     
-    InitWindow(GetMonitorWidth(1), GetMonitorHeight(1), "TheBugger");
-    ToggleFullscreen();
+    InitWindow(1920, 1080, "TheBugger");
+    //ToggleFullscreen();
     int wid = GetScreenWidth();
     int hei = GetScreenHeight();
     InitAudioDevice();
@@ -171,7 +171,7 @@ int main()
             while (IsKeyUp(KEY_P))
             {
                 //Atualizando a stream da música
-                HideCursor();
+                //HideCursor();
 
                 UpdateMusicStream(music);
 
@@ -179,7 +179,7 @@ int main()
                 criaturasVivas = wave * 5;
                 while (criaturasVivas > 0)
                 {
-                    DisableCursor();
+                    //DisableCursor();
                     //Atualizando a stream da música
                     UpdateMusicStream(music);
                     //Criando a parte de imagem
@@ -373,6 +373,7 @@ int main()
                     //cameraJogador.zoom = 1.1f; //0.75 paradao 
 
                     //Atualizando a mira
+                    SetMouseScale(1.0f, 1.0f);
                     miraPosicao = GetMousePosition();
                     miraPosicao.x += cameraJogador.target.x - cameraJogador.offset.x;
                     miraPosicao.y += cameraJogador.target.y - cameraJogador.offset.y;
